e3c3dd9d07b9944c4e9ceed660c92259c770c55a
mherre18 <manulilla83@gmail.com>
2020-04-13 21:19:23 -0500

    variables

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let DB;
+
+const form = document.querySelector('form');
+const namePet = document.querySelector('#pet');
+const nameClient = document.querySelector('#client');
+const phone = document.querySelector('#phone');
+const date = document.querySelector('#date');
+const time = document.querySelector('#time');
+const symptoms = document.querySelector('#symptoms');
+const dates = document.querySelector('#dates');
+const headinAdmin = document.querySelector('#admin');
